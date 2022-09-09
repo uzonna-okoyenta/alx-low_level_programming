6fa680b336bafc508ed81976350a71ebdfa49451
uzonna-okoyenta <uzonnaok@gmail.com>
2022-09-08 21:45:39 -0700

    wowo

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+/**
+ *
+ *main - main block
+ *
+ *   * Description - using the function main
+ *
+ *Return: 0
+ *
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
