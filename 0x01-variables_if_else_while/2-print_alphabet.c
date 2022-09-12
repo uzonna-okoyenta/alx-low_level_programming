c6aa4cfdbdb0772d2468056eb2f0629f5175bbed
uzonna-okoyenta <uzonnaok@gmail.com>
2022-09-12 16:09:48 -0700

    wowo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * Description - code to display lowercase alphabets
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str = "abcdefghijklmno
+
+			"pqrstuvwxyz";
+		puts(str);
+
+	return (0);
+}
