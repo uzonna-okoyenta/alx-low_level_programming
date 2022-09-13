c3e2708a9bfb9e7b26a08c85d35e286190bd4936
uzonna-okoyenta <uzonnaok@gmail.com>
2022-09-12 17:41:29 -0700

    wowo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: A program that prints all single digit up to 10
+ *Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
